1cd3d9c318f4a8ad34da7edfbfc270d4749abfa5
jisooround <9utesleben@gmail.com>
2022-10-29 23:49:12 +0900

    fix : 강사님 풀이 추가

--- Inflearn/section1/2_삼각형 판별하기.js
+++ Inflearn/section1/2_삼각형 판별하기.js
@@ -8,3 +8,14 @@ function solution(a, b, c){
 }
 
 console.log(solution(6, 7, 11));
+
+// 강사님 풀이
+function solution(a, b, c){
+  let answer="YES", max;
+  let tot=a+b+c;
+  if(a>b) max=a;
+  else max=b;
+  if(c>max) max=c;
+  if(tot-max<=max) answer="NO"; 
+  return answer;
+}
